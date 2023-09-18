9379c8f4d7c78748efa0280d5628c034d0299f2b
Kandy44 <nkandy44@gmail.com>
2023-09-19 03:41:06 +0530

    Fixed type checking.

--- fib_py/fib_calcs/fib_numbers.py
+++ fib_py/fib_calcs/fib_numbers.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Optional
 from .fib_number import iter_fib_number
 
 
-def calculate_numbers(numbers: List[int]) -> List[int]:
+def calculate_numbers(numbers: List[int]) -> List[Optional[int]]:
     return [iter_fib_number(n=n) for n in numbers]
